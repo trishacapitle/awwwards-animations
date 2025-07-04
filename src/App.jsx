8e126f78e7075af92259ff1e61343d2f60e698ec
trishacapitle <140716826+trishacapitle@users.noreply.github.com>
2025-07-04 19:43:30 +0800

    feat: lay groundwork for page components and added GSAP scrollsmoother for smooth scrolling

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import Hero from "./pages/Hero";
+import Loader from "./pages/Loader";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { useRef } from "react";
+
+const App = () => {
+	gsap.registerPlugin(useGSAP, ScrollSmoother);
+  const smoother = useRef();
+  const main = useRef();
+	useGSAP(
+		() => {
+			smoother.current = ScrollSmoother.create({
+				smooth: 2,
+				effects: true,
+			});
+		},
+		{ scope: main }
+	);
+
+  return (
+		<div id="smooth-wrapper" ref={main}>
+			<div id="smooth-content">
+				<Hero />
+				<Loader />
+			</div>
+		</div>
+	);
+};
+
+export default App;
